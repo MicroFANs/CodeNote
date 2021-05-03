9aecbdfb586b1701c858a2bfc569490c2fef4f13
FAN <f_zx1995@163.com>
2021-05-04 00:42:04 +0800

    62. 不同路径

--- src/LeetCode/leetcode62.java
+++ src/LeetCode/leetcode62.java
@@ -6,24 +6,24 @@ package LeetCode;
  */
 public class leetcode62 {
     public static int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
+        int[][] dp = new int[m][n];
 
-        for (int i = 0; i <m ; i++) {
-            for (int j = 0; j <n ; j++) {
-                if(i==0){
-                    dp[0][j]=1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0) {
+                    dp[0][j] = 1;  //初始化dp数组
+                } else if (j == 0) {
+                    dp[i][0] = 1;  //初始化dp数组
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; //状态转移方程
                 }
-                else if(j==0){
-                    dp[i][0]=1;
-                }
-                else
-                {dp[i][j]=dp[i-1][j]+dp[i][j-1];}
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
-        System.out.println(uniquePaths(7,3));
+        System.out.println(uniquePaths(7, 3));
     }
 }
