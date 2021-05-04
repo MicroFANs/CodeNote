e7bf6352fc8d967c749da0ff2c7648bc7f9893fb
FAN <f_zx1995@163.com>
2021-05-04 15:20:00 +0800

    6. Z 字形变换

--- src/LeetCode/leetcode6.java
+++ src/LeetCode/leetcode6.java
@@ -16,9 +16,6 @@ public class leetcode6 {
                 res.append(s.charAt(j + i));
                 if (i != 0 && i != numRows - 1 && j + 2*numRows-2 - i < len)
                     res.append(s.charAt(j + 2*numRows-2 - i));
-
-
-//
             }
         }
         return res.toString();
